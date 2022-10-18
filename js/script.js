b8645580b1ca99c6af01de84b413173481cf36e1
Madhuja93 <madhujapabasara@gmail.com>
2022-10-18 17:25:42 +0530

    updated forms with jquery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-function showMenu(){
+function showMenu() {
   var navLinks = document.getElementById("navLinks");
-  navLinks.style.right ="0";
+  navLinks.style.right = "0";
 }
 function hideMenu() {
   var navLinks = document.getElementById("navLinks");
   navLinks.style.right = "-200px";
+}
+
+function onFormSubmit() {
+  var firstname = $("#firstName").val();
+  var lastname = $("#firstName").val();
+  var Email = $("#email").val();
+  var Password = $("#password").val();
+
+  var userInfo = {
+    _firstName: firstname,
+    _lastName: lastname,
+    _email: Email,
+    _password: Password,
+  };
+
+  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+
+  window.location.assign("readers.html");
 }
