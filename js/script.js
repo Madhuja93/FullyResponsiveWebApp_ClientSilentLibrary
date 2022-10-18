a239980b2e31361389ef4853ab061d48d5f0b970
Madhuja93 <madhujapabasara@gmail.com>
2022-10-18 18:35:06 +0530

    Added JS CRUD Operations

--- js/script.js
+++ js/script.js
@@ -24,3 +24,60 @@ function onFormSubmit() {
 
   window.location.assign("readers.html");
 }
+
+function onFormUpdate() {
+  var firstname = $("#firstName").val();
+  var lastname = $("#firstName").val();
+  var Email = $("#email").val();
+  var Password = $("#password").val();
+
+  var userInfo = {
+    _firstName: firstname,
+    _lastName: lastname,
+    _email: Email,
+    _password: Password,
+  };
+
+  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+
+  window.location.assign("readers.html");
+}
+
+function onUpdateButtonClick() {
+  $(".userinfo").addClass("update-btn");
+  $(".loginregister").removeClass("update-btn");
+
+  var userInfo = sessionStorage.getItem("readerInfo");
+  var userInfoJSON = JSON.parse(userInfo);
+  $("#firstName_0").val(userInfoJSON._firstName);
+  $("#lastName_0").val(userInfoJSON._lastName);
+  $("#email_0").val(userInfoJSON._email);
+  $("#password_0").val(userInfoJSON._password);
+}
+
+function onDeleteButtonClick() {
+  sessionStorage.setItem("readerInfo", null);
+  alert("Successfully Deleted");
+
+  window.location.assign("register.html");
+}
+
+function onUpdateRecord() {
+  var firstname = $("#firstName_0").val();
+  var lastname = $("#lastName_0").val();
+  var Email = $("#email_0").val();
+  var Password = $("#password_0").val();
+
+  var userInfo = {
+    _firstName: firstname,
+    _lastName: lastname,
+    _email: Email,
+    _password: Password,
+  };
+
+  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+
+  alert("Successfully Updated!");
+
+  window.location.assign("readers.html");
+}
