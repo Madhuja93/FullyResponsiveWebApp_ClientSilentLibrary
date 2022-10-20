57499a954caf9b9de9d89cbd75f436bd7c71c7b3
Madhuja93 <madhujapabasara@gmail.com>
2022-10-20 21:13:25 +0530

    updated login accounts

--- js/script.js
+++ js/script.js
@@ -8,24 +8,31 @@ function hideMenu() {
 }
 
 function onFormSubmit() {
+  
   var firstname = $("#firstName").val();
   var lastname = $("#firstName").val();
   var Email = $("#email").val();
   var Password = $("#password").val();
 
+  var generatedId = localStorage.length + 1;
+
   var userInfo = {
+    _Id: generatedId,
     _firstName: firstname,
     _lastName: lastname,
     _email: Email,
     _password: Password,
   };
 
-  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+  var userInfo = JSON.stringify(userInfo);
 
-  window.location.assign("readers.html");
+  localStorage.setItem("readerInfo_" + generatedId, userInfo);
+
+  window.location.assign("readers.html?Id=readerInfo_" + generatedId);
 }
 
 function onFormUpdate() {
+  
   var firstname = $("#firstName").val();
   var lastname = $("#firstName").val();
   var Email = $("#email").val();
@@ -38,7 +45,9 @@ function onFormUpdate() {
     _password: Password,
   };
 
-  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+  var userInfo = getreaderInfoRecord();
+
+  localStorage.setItem("readerInfo_", userInfo_Id);
 
   window.location.assign("readers.html");
 }
@@ -47,37 +56,64 @@ function onUpdateButtonClick() {
   $(".userinfo").addClass("update-btn");
   $(".loginregister").removeClass("update-btn");
 
-  var userInfo = sessionStorage.getItem("readerInfo");
-  var userInfoJSON = JSON.parse(userInfo);
-  $("#firstName_0").val(userInfoJSON._firstName);
-  $("#lastName_0").val(userInfoJSON._lastName);
-  $("#email_0").val(userInfoJSON._email);
-  $("#password_0").val(userInfoJSON._password);
+  var userInfo = getreaderInfoRecord();
+  $("#firstName_0").val(userInfo._firstName);
+  $("#lastName_0").val(userInfo._lastName);
+  $("#email_0").val(userInfo._email);
+  $("#password_0").val(userInfo._password);
 }
 
 function onDeleteButtonClick() {
-  sessionStorage.setItem("readerInfo", null);
+  
+  var userInfo = getreaderInfoRecord();
+  localStorage.removeItem("readerInfo_" + userInfo._Id);
   alert("Successfully Deleted");
 
   window.location.assign("register.html");
 }
 
 function onUpdateRecord() {
+  
   var firstname = $("#firstName_0").val();
   var lastname = $("#lastName_0").val();
   var Email = $("#email_0").val();
   var Password = $("#password_0").val();
 
+  var existingUser = getreaderInfoRecord();
+
   var userInfo = {
+    _Id: existingUser._Id,
     _firstName: firstname,
     _lastName: lastname,
     _email: Email,
     _password: Password,
   };
 
-  sessionStorage.setItem("readerInfo", JSON.stringify(userInfo));
+  localStorage.setItem(
+    "readerInfo_" + existingUser._Id,
+    JSON.stringify(userInfo)
+  );
 
   alert("Successfully Updated!");
 
-  window.location.assign("readers.html");
+  window.location.assign("readers.html?Id=readerInfo_" + existingUser._Id);
+}
+
+function getreaderInfoRecord() {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var userId = url.searchParams.get("Id");
+
+  var _userInfos = localStorage;
+  var _userInfo;
+  for (var i = 0; i < _userInfos.length; i++) {
+    if (
+      userId ==
+      "readerInfo_" + JSON.parse(localStorage.getItem(userId))._Id
+    ) {
+      _userInfo = JSON.parse(localStorage.getItem(userId));
+    }
+  }
+
+  return _userInfo;
 }
